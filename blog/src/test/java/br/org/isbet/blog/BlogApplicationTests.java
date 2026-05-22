43fad0690ca16f22b78926438bcc9610941f0d5d
nabegod <anabeatrizsouzaalves94@gmail.com>
2026-05-22 17:12:08 -0300

    update

---
+++ blog/src/test/java/br/org/isbet/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.isbet.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
